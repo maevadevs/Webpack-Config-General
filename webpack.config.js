787a437de95102471ef20fa79a717ea8e2f82616
maevadevs <maevadevs@users.noreply.github.com>
2019-02-12 09:00:00 -0600

    Setup dev and prod env

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,58 @@
+// DEPENDENCIES
+// ************
+
 require('dotenv').config()
 const { join } = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { WDS_HOST, WDS_PORT } = process.env
 
-module.exports = {
-  entry: {
-    main: './src/index.js'
-  }, 
-  output: {
-    filename: 'main.js',
-    path: join(__dirname, 'dist')
-  },
-  plugins: [
-    // Auto-generate index.html
-    new HtmlWebpackPlugin({
-      title: 'Webpack Demo',
-    }),
-  ],
-  // Settings for webpack-dev-server
-  devServer: {
-    host: process.env.WDS_HOST, // Defaults to `localhost`
-    port: process.env.WDS_PORT, // Defaults to 8080
-    open: false, // Automatically open the page in browser
-    historyApiFallback: true, // Using HTML5 History API based routing
-    overlay: true, // Error overlay to capture compilation related warnings and errors
-    contentBase: join(__dirname, 'dist') // Contents not passing through webpack are served directly from this folder
-    // proxy: { "/api": "http://my.api/endpoint" } // If using multiple servers, proxy WDS to them
-    // header: // Attach custom headers to your requests here
-  }
-} 
+// WEBPACK CONFIG
+// **************
+
+// --- ENTRY / OUTPUT ---
+// **********************
+
+const entry = { 
+  main: './src/index.js' 
+}
+const output = {
+  filename: 'main.js',
+  path: join(__dirname, 'dist')
+}
+
+// --- PLUGINS ---
+// ***************
+
+const plugins = [
+  // Auto-generate index.html
+  new HtmlWebpackPlugin({
+    title: 'Webpack Demo',
+  })
+]
+
+// --- WEBPACK-DEV-SERVER ---
+// **************************
+
+const devServer = {
+  host: WDS_HOST, // Defaults to `localhost`
+  port: WDS_PORT, // Defaults to 8080
+  open: false, // Automatically open the page in browser
+  historyApiFallback: true, // Using HTML5 History API based routing
+  overlay: true, // Error overlay to capture compilation related warnings and errors
+  contentBase: join(__dirname, 'dist') // Contents not passing through webpack are served directly from this folder
+  // proxy: { "/api": "http://my.api/endpoint" } // If using multiple servers, proxy WDS to them
+  // header: // Attach custom headers to your requests here
+}
+
+// EXPORT CONFIG
+// *************
+// 'process.env' is passed to the function during the build call
+// Any other arguments during the call is passed as argv
+
+module.exports = (env, argv) => ({
+  mode: env,
+  entry, 
+  output,
+  plugins,
+  devServer: env === 'production' ? {} : devServer
+})
